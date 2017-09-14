cb1c3fcee7565f0c48e54f888c47889811e207e1
JacqueWhite <jacquecwhite@gmail.com>
2017-09-14 17:06:14 -0500

    adding images and fixing table bug

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Bamazon
+Overview
 An Amazon-like storefront utilizing MySQL.  The app takes in orders from customers and depletes stock from the store's inventory.
+
+Bamazon is a CLI store that takes you through the following process:
+
+1. Welcome to Bamazon, here are the products:
+[![Alt text](images/welcomescreen.png)](#)
+
+2. Next, enter Product ID to purchase:
+[![Alt text](images/qty.png)](#)
+
+3. A function runs to check inventory before the purchase runs.   
+[![Alt text](images/whatnext.png)](#)
+
+4. If there is enough in the inventory, the purchase is completed and you can then SHOP or EXIT.
+[![Alt text](images/whatnext.png)](#)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require('cli-table');
+var chalk = require('chalk');
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -17,17 +18,21 @@ connection.connect(function(err) {
 });
 
 function start() {
+	var table = "";
 	readProducts();
-
 }
 
-var table = new Table({
+function readProducts() {
+
+  var table = new Table({
     head: ['ID', 'Name', 'Price'],
     colWidths: [5, 40, 10]
-});
+	});	
 
-function readProducts() {
-  console.log("Showing all products...\n");
+  console.log(chalk.red("---------------------------------------------"));
+  console.log(chalk.red("Welcome to BAMAZON!\n"));
+  console.log(chalk.red("---------------------------------------------"));
+  console.log("Here are the current products...\n");
   connection.query("SELECT * FROM products",
 	function(err, res) {
 		if (err) {
@@ -36,84 +41,70 @@ function readProducts() {
       		for (var i = 0; i < res.length; i++) {
                 table.push([res[i].item_id, res[i].product_name, res[i].price]);
       		}
-      		console.log(table.toString());
         }
+        console.log(table.toString());
         buyProducts();
 	})
 }
 
-	function buyProducts() {
-	  inquirer
-	    .prompt([
-	      {
-	      name: "item",
-	      type: "input",
-	      message: "To purchase an item, enter the ID: "
-	      },
-	      {
-	      name: "stock",
-	      type: "input",
-	      message: "How many would you like to purchase?"
-	      }
-	    ])
-	    .then(function(answer) {
-	        console.log('Order received!');
-	        
-	        connection.query("SELECT * FROM products WHERE ?", { item_id: answer.item}, function(err, res) {
-					console.log("ID # " + res[0].item_id + " Product: " + res[0].product_name + " Price: " + res[0].price);
+function buyProducts() {
+  inquirer
+    .prompt([
+      {
+      name: "item",
+      type: "input",
+      message: "To purchase an item, enter the ID: "
+      },
+      {
+      name: "stock",
+      type: "input",
+      message: "How many would you like to purchase?"
+      }
+    ])
+    .then(function(answer){
+    console.log("Item ID: " + answer.item);
+    console.log("QTY: " + answer.stock);
 
-				if (answer.stock > res[0].stock_quantity) {
-				console.log("Please chose fewer items, or select another product.");
-
-				} else {
-
-					console.log('Order on its way!');
-					connection.query('UPDATE products SET ? WHERE ?', 
-					[
-						{ 
-							stock_quantity: (res[0].stock_quantity - answer.stock) 
-						}, 
-						{ 
-							item_id: res[0].id 
-						}
-					],
-
-					function(err, result) {
-					console.log("You bought " + stock + " " + res[0].product_name + "(s) today! That leaves " + (res[0].stock_quantity - answer.stock) + " in stock!");
-					});
-				}
-			}
+        connection.query("SELECT stock_quantity FROM products WHERE item_id = ?",[answer.item], function(err, res){
+            // console.log("All results: " + res);
+            console.log("Items left in Inventory: " + res[0].stock_quantity);
+       
+            if(res[0].stock_quantity > answer.stock){
+               connection.query("UPDATE products SET ? WHERE ?",
+               [
+                {
+                   stock_quantity: (res[0].stock_quantity-answer.stock)
+                },
+                {
+                   item_id: answer.item
+                }
+               ]); 
+               console.log("---------------------------------------------");
+               console.log("*****Order received! Thank you!*****");
+               console.log("---------------------------------------------");
+               whatNext();
+                
+            } else {
+                console.log("******Sorry, Insufficiant Inventory******");
+                whatNext();
+            }
 		});
-	}); 
+	});
+}; 
 
-
-// I had to move the function inside the answer function due to scope...
-
-// function checkingInventory(stock, res) {
-// 	console.log("ID # " + res[0].item_id + " Product: " + res[0].product_name + " Price: " + res[0].price);
-// 	if (answer.stock > res[0].stock_quantity) {
-// 		console.log("Please chose fewer items, or select another product.");
-// 	} else {
-//         console.log('Order on its way!');
-//         connection.query(
-//             'UPDATE products SET ? WHERE ?', 
-//             [
-// 	            { 
-// 	            	stock_quantity: (res[0].stock_quantity - answer.stock) 
-// 	            }, 
-// 	            { 
-// 	            	item_id: res[0].id 
-// 	            }
-//             ],
-//             function(err, result) {
-//                 console.log("You bought " + stock + " " + res[0].product_name + "(s) today! That leaves " + (res[0].stock_quantity - answer.stock) + " in stock!");
-//             })
-//     }
-// }
-
-// changing users...
-// connection.changeUser({user : 'user2'}, function(err) {
-//   if (err) throw err;
-// });
-
-// connection.end();
+function whatNext(){
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "whatNext",
+            message: "What would you like to do?",
+            choices: ["Shop", "Exit Bamazon"]
+        }
+    ]).then(function(answer){
+        if(answer.whatNext === "Shop"){
+            readProducts();
+        }else if(answer.whatNext === "Exit Bamazon"){
+            connection.end();
+        }
+    });
+}
